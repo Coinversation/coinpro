72baa0d1608df57a73b4abf756f0d535500e4a4f
bwhour <juvemeen.xio@gmail.com>
2021-03-31 17:20:40 +0800

    add exchange code

---
+++ contracts/exchangeproxy/build-all.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -eu
+
+cargo +nightly contract build --manifest-path cdot/Cargo.toml
+cargo +nightly contract build --manifest-path pool/Cargo.toml
+cargo +nightly contract build
