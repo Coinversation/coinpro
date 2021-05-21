d052beffc5de9b728adeaec37fe4abc94df8679f
FleetingTimePaul <83365885@qq.com>
2021-05-21 14:49:53 +0800

    update

--- contracts/liquiditypool/token/lib.rs
+++ contracts/liquiditypool/token/lib.rs
@@ -252,23 +252,4 @@ mod token {
             self.controller = manager;
         }
     }
-
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// We test if the default constructor does its job.
-        #[test]
-        fn default_works() {
-        }
-
-        /// We test a simple use case of our contract.
-        #[test]
-        fn it_works() {
-        }
-    }
 }
